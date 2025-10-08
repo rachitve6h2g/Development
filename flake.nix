e6c56cad61b3ed25aef6971687f96e5cb67f7d42
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-10-09 00:14:05 +0530

    add: clang-tools for devShell

--- flake.nix
+++ flake.nix
@@ -3,24 +3,29 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
     let
-      system = "x86_64-linux";  # adjust for your system
+      system = "x86_64-linux"; # adjust for your system
       pkgs = import nixpkgs { inherit system; };
-    in {
+    in
+    {
       devShells.${system}.default = pkgs.mkShell {
         name = "c-dev-shell";
         packages = [
           pkgs.gcc
-          pkgs.gnumake  # optional, but usually needed
-          pkgs.gdb      # optional, for debugging
+          pkgs.gnumake # optional, but usually needed
+          pkgs.gdb # optional, for debugging
+          pkgs.clang-tools
         ];
 
-	shellHook = ''
-		  alias ec='emacsclient -c'
-		  echo "C dev environment ready to use"
-	'';
+        shellHook = ''
+          		  alias ec='emacsclient -c'
+          		  echo "C dev environment ready to use"
+          	'';
       };
     };
 }
-
